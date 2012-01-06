4b9bf025fe58cb9480e368000c358af9f915f7f5
Matt Cockayne <matt@zucchi.co.uk>
2012-01-06 14:50:04 +0000

    Initial commit of SilverChimp

---
+++ README.md
@@ -0,0 +1,31 @@
+# SilverChimp - Simple MailChimp Integration for SilverStripe
+
+## Introduction
+
+A simple integration with MailChimp allowing you to create a Subscribe to newsletter page.
+
+This was created to allow simple mailing list subscription and allowing you to utilise MailChimps own form/merge fields.
+
+## Maintainer Contact
+
+  * Matt Cockayne  
+    <matt@zucchi.co.uk>
+    
+## Requirements
+
+ * Silverstripe 2.4 or newer
+
+## Installation
+
+ * Copy the 'SilverChimp' directory into you main SilverStripe webroot
+ * Add the line `SilverChimpSettings::$api_key = '<api-key>';` to mysite/_config.php. (replacing <api-key> with your own key)
+ * trigger a build at /dev/build?flush=1
+ 
+ ## Usage
+ 
+ You should now be able to create a new "Silver Chimp Subscription Page". You can now select the list you wish the page to subscribe users to.
+ 
+ When selecting a list and saving you will be able to review the fields that have been set up in MailChimps own system.
+ 
+ Shoudl you wish to manage the fields used for the mailing list you must do so from your MailChimp account
+  
---
+++ code/SilverChimpSettings.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * SilverChimp settings Container
+ * @author Matt Cockayne <matt@zucchi.co.uk>
+ * @package SilverChimp
+ */
+class SilverChimpSettings {
+
+    /**
+     * the MailChimp API Key to use
+     * 
+     * @var string
+     */
+    public static $api_key = null;
+    
+    /**
+     * Shoud a secure connectoin be used when communicating 
+     * with MailChimp
+     * 
+     * @var boolean
+     */
+    public static $secure_connection = false;
+    
+    /**
+     * email type preference for the email (html, text, 
+     * or mobile defaults to html)
+     * @var string
+     */
+    public static $email_type = 'html';
+    
+    /**
+     * flag to control whether a double opt-in confirmation 
+     * message is sent, defaults to true.
+     * 
+     * @var unknown_type
+     */
+    public static $double_opt_in = false;
+    
+    /**
+     * if your double_optin is false and this is true, we will 
+     * send your lists Welcome Email if this subscribe succeeds 
+     * - this will *not* fire if we end up updating an existing 
+     * subscriber. If double_optin is true, this has no effect.
+     * 
+     * @var unknown_type
+     */
+    public static $send_welcome = false;
+   
+}
