2a0e0751cbdfa9e0166e4f8c5a279e40ca35d6e7
Matt Cockayne <matt@zucchi.co.uk>
2012-01-17 16:30:52 +0000

    Added support for mailchimp groupings

--- code/SilverChimpSettings.php
+++ code/SilverChimpSettings.php
@@ -24,6 +24,7 @@ class SilverChimpSettings {
     /**
      * email type preference for the email (html, text, 
      * or mobile defaults to html)
+     * 
      * @var string
      */
     public static $email_type = 'html';
@@ -32,7 +33,7 @@ class SilverChimpSettings {
      * flag to control whether a double opt-in confirmation 
      * message is sent, defaults to true.
      * 
-     * @var unknown_type
+     * @var boolean
      */
     public static $double_opt_in = false;
     
@@ -42,8 +43,24 @@ class SilverChimpSettings {
      * - this will *not* fire if we end up updating an existing 
      * subscriber. If double_optin is true, this has no effect.
      * 
-     * @var unknown_type
+     * @var boolean
      */
     public static $send_welcome = false;
+    
+    /**
+     * flag to control whether a existing subscribers should be updated 
+     * instead of throwing and error, defaults to false
+     * 
+     * @var boolean
+     */
+    public static $update_existing = false;
    
+    /**
+     * flag to determine whether we replace the interest groups with 
+     * the groups provided, or we add the provided groups to the 
+     * member's interest groups, defaults to false
+     * 
+     * @var boolean
+     */
+    public static $replace_groups = false;
 }
